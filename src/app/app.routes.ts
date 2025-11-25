6fb16ccdc943087aa5732a97d2448f3f13e1ce2e
GGmery <grungemxry@gmail.com>
2025-11-25 20:26:44 +0100

    Creación del navbar y footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,20 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { Home } from './pages/home/home';
+import { About} from './pages/about/about';
+import { Games } from './pages/games/games';
+import { Contact } from './pages/contact/contact';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },          // Inicio
+  { path: 'about', component: About },    // Sobre nosotros
+  { path: 'games', component: Games },    // Nuestros juegos
+  { path: 'contact', component: Contact }, // Contacto
+  { path: '**', redirectTo: '' }                   // Ruta por defecto (404 → Inicio)
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule {}
---
+++ src/app/shared/navbar/navbar.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  standalone: true,
+  imports: [RouterLink, RouterLinkActive],
+  templateUrl: './navbar.html',
+  styleUrls: ['./navbar.css']
+})
+export class NavbarComponent {
+  isDarkMode = false;
+
+  toggleTheme() {
+    this.isDarkMode = !this.isDarkMode;
+    if (this.isDarkMode) {
+      document.body.classList.add('dark-mode');
+    } else {
+      document.body.classList.remove('dark-mode');
+    }
+  }
+}
