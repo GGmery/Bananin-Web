ce1ea46baa48657237eca3473cad9751723aaa8b
GGmery <grungemxry@gmail.com>
2025-12-15 20:13:02 +0100

    Alpha gamer añadido

--- src/app/pages/about/about.ts
+++ src/app/pages/about/about.ts
@@ -2,10 +2,32 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  imports: [],
   templateUrl: './about.html',
-  styleUrl: './about.css',
+  styleUrls: ['./about.css']
 })
 export class About {
+  countdown: string = '';
 
+  constructor() {
+    this.updateCountdown();
+    setInterval(() => this.updateCountdown(), 1000); // actualiza cada segundo
+  }
+
+  private updateCountdown(): void {
+    const endDate = new Date("2026-01-01T00:00:00");
+    const now = new Date();
+    const diff = endDate.getTime() - now.getTime();
+
+    if (diff <= 0) {
+      this.countdown = "¡Ya terminó la condena en ZZZ!";
+      return;
+    }
+
+    const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+    const minutes = Math.floor((diff / (1000 * 60)) % 60);
+    const seconds = Math.floor((diff / 1000) % 60);
+
+    this.countdown = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+  }
 }
