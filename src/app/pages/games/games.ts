a349353e6425f5162e2e4ac90e66982de3798cb3
GGmery <grungemxry@gmail.com>
2025-12-16 19:17:38 +0100

    Actualizada la pestaña de nuestros juegos

--- src/app/pages/games/games.ts
+++ src/app/pages/games/games.ts
@@ -1,11 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import gsap from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 
 @Component({
   selector: 'app-games',
-  imports: [],
   templateUrl: './games.html',
-  styleUrl: './games.css',
+  styleUrls: ['./games.css']
 })
-export class Games {
+export class Games implements AfterViewInit {
 
+  ngAfterViewInit(): void {
+    // Registrar plugin de GSAP
+  gsap.registerPlugin(ScrollTrigger);
+
+    gsap.utils.toArray('.mini-card').forEach((card: any, i: number) => {
+      gsap.from(card, {
+        scrollTrigger: {
+          trigger: card,
+          start: 'top 85%',
+          toggleActions: 'play none none none'
+        },
+        opacity: 0,
+        y: 50,
+        duration: 1,
+        delay: i * 0.1,
+        ease: 'power3.out'
+      });
+    });
+  }
 }
